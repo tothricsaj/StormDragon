ded12b422f7dd51d17b37e707d86e04a81a42881
tothricsaj <tothricsaj@gmail.com>
2024-05-02 10:43:44 +0200

    feat: changed architecture diagram

--- web/src/Light.tsx
+++ web/src/Light.tsx
@@ -1,18 +1,19 @@
 type Props = {
   /** some description */
-  variant: 'green' | 'yellow' | 'red'
+  variant: 'green' | 'yellow' | 'red',
+  size?: number
 };
 
 /** Some comment about my Light component */
-export const Light = ({variant = 'green'}: Props) => {
+export const Light = ({variant = 'green', size = 50}: Props) => {
   return (
     <div style={{
       background: variant,
       borderRadius: '50%',
-      width: 50,
-      height: 50
+      width: size,
+      height: size
     }}></div>
-  )
-}
+  );
+};
 
 export default Light;
